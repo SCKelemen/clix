ef6356de3182a698d17cd7d12ed89a127756d262
Samuel Kelemen <samuel@kelemen.com>
2025-10-31 15:17:04 +0100

    Update help extension example test

--- ext/help/example_test.go
+++ ext/help/example_test.go
@@ -7,19 +7,18 @@ import (
 
 func ExampleExtension() {
 	app := clix.NewApp("example")
-	
+
 	// Add the help extension to enable help command
 	app.AddExtension(help.Extension{})
-	
+
 	root := clix.NewCommand("example")
 	app.Root = root
-	
+
 	// Now the app will have:
 	//   example help          - Show root help
 	//   example help [command] - Show command help
-	// 
+	//
 	// Flag-based help still works without the extension:
 	//   example -h, example --help
 	//   example command -h, example command --help
 }
-
